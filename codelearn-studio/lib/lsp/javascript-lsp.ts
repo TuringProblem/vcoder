cf4f977b959981729dace7ab9bcd67f4be3139ed
Override <122590935+TuringProblem@users.noreply.github.com>
2025-08-08 23:05:50 -0400

    fix: delete files

--- codelearn-studio/lib/lsp/javascript-lsp.ts
+++
@@ -1,5 +0,0 @@
-"use client"
-
-// JS reuses the same TS language service functions in Monaco (javascriptDefaults)
-// This module is here for future extensibility (external LSPs).
-export { getHoverAt, renameSymbolAtCursor, goToDefinition, findReferences, collectDiagnostics } from "./typescript-lsp"
